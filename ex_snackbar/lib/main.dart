c23cf9715a2d8a42916a4b7c40b8c5838e6d6794
hhs0579 <hhs0579@naver.com>
2021-11-08 19:22:11 +0900

    lion

--- ex_snackbar/lib/main.dart
+++ ex_snackbar/lib/main.dart
@@ -14,6 +14,8 @@ class MyApp extends StatelessWidget {
 }
 
 class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +23,103 @@ class MyPage extends StatelessWidget {
         title: Text('Snack Bar'),
         centerTitle: true,
       ),
-      body:Center(
-        child: FlatButton,
-      )
+      body: HomeBody(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.thumb_up),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Like a new Snack bar'),
+              duration: Duration(seconds: 5),
+              action: SnackBarAction(
+                label: 'Undo',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ThirdPage()),
+                  );
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class HomeBody extends StatelessWidget {
+  const HomeBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+          child: Text('Go to the second page'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          }),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Center(
+        child: Text(
+          '"좋아요가 추가 되었습니다"',
+          style: TextStyle(fontSize: 20.0, color: Colors.redAccent),
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Third Page'),
+      ),
+      body: Builder(
+        builder: (context) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '"좋아요를 취소 하시겠습니까?"',
+                  style: TextStyle(fontSize: 20.0, color: Colors.redAccent),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content:Text('"좋아요가 취소 되었습니다"'),
+                        duration: Duration(seconds:3),
+                      ),
+                    );
+                  },
+                  child: Text('취소하기'),
+                ),
+              ],
+            ),
+          );
+        }
+      ),
     );
   }
 }
---
+++ lion/lib/main.dart
@@ -0,0 +1,132 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'BBANTO',
+      home:Grade(),
+    );
+  }
+}
+class Grade extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:Colors.amber[800],
+      appBar: AppBar(
+        title:Text("BBANTO"),
+        backgroundColor:Colors.amber[700],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body:Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+        child:Column(
+          crossAxisAlignment:CrossAxisAlignment.start,
+          children: <Widget> [
+          Center(
+            child: CircleAvatar(
+              backgroundImage:AssetImage('assets/lion.jpg'),
+              radius: 60.0,
+            ),
+          ),
+          
+            Text('Name',
+            style: TextStyle(
+              color:Colors.white,
+              letterSpacing: 2.0,
+            ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('BBANTO',
+            style: TextStyle(
+              color:Colors.white,
+              letterSpacing:2.0,
+              fontSize:28.0,
+              fontWeight:FontWeight.bold
+            ),
+            ),
+            SizedBox(
+              height:30.0,
+            ),
+            Text('BBANTO POWER LEVEL',
+            style: TextStyle(
+              color:Colors.white,
+              letterSpacing: 2.0,
+            ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            // ignore: prefer_const_constructors
+            Text('14',
+            style: TextStyle(
+              color:Colors.white,
+              letterSpacing:2.0,
+              fontSize:28.0,
+              fontWeight:FontWeight.bold
+            ),
+            ),
+            SizedBox(
+              height:30.0,
+            ),
+            Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: <Widget> [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  height:10.0,
+                  ),
+                Text('using lightsaber',
+                style: TextStyle(
+                  fontSize:16.0,
+                  letterSpacing:1.0,
+                ),
+                ),
+              ],
+              ),
+              Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: <Widget> [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  height:10.0,
+                  ),
+                Text('face hero tatoo',
+                style: TextStyle(
+                  fontSize:16.0,
+                  letterSpacing:1.0,
+                ),
+                ),
+              ],
+              ),
+              Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: <Widget> [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  height:10.0,
+                  ),
+                Text('fire flames',
+                style: TextStyle(
+                  fontSize:16.0,
+                  letterSpacing:1.0,
+                ),
+                ),
+              ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
